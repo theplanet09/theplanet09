a0a7a2e7ec7df79f5d85c720f510f5f7bc8e7d35
somebulb <69670938+somebulb@users.noreply.github.com>
2022-02-25 23:47:03 -0600

    major commit; got a pretty simple landing page running; banana form doesn't take anything yet, keys aren't hidden and must be revoked;

--- planet-09/my_portfolio/urls.py
+++ planet-09/my_portfolio/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('banana.urls')),
-    path('defaultsite', include('banana.urls'))
+    path('defaultsite/', include('banana.urls'))
 ]
