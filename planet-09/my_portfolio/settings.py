ce8e26abcd17f1d07b91a66131427f693d0945f3
somebulb <69670938+somebulb@users.noreply.github.com>
2022-02-25 23:01:22 -0600

    major commit; got a pretty simple landing page running; banana form doesn't take anything yet, keys aren't hidden and must be revoked;

--- planet-09/my_portfolio/settings.py
+++ planet-09/my_portfolio/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5319dvt13#=3e0p9wo4*a=pw=9@d9(+_j&6_$ynbicisb1km=8'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
